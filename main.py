b47a47201bc4e80f219e92d506462aed4086bb87
Jordan Lafferty <jordanlafferty99@yahoo.com>
2022-02-07 17:55:17 -0600

    Optimization

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import csv
+import timeit
+
 
 
 class Graph:
@@ -11,10 +13,10 @@ class Graph:
             theReader = csv.reader(file, delimiter=',')
             for rows in theReader:
                 r = []
-                for x in rows:
-                    r.append(int(x))
+                for y in rows:
+                    r.append(int(y))
                 v.append(r)
-        return v;
+        return v
 
     def read(self, v):  # graph.read([])
         self.data = v
@@ -22,7 +24,12 @@ class Graph:
 
     def findMinimum(self, E):  # how to optimize HW
         val = E[0]
+        #p = val[[2]].idxmin()
+        #val = E[p]
+        #x = sorted(E, key=itemgetter(2))
         for i in range(len(E)):
+            #if E[i][2] == 1:
+               # return E[i]
             if val[2] > E[i][2]:
                 val = E[i]
         return val
@@ -55,7 +62,82 @@ class Graph:
         print("MSP length is: ", length)
 
 
+    #not correct
+    def process2(self):
+        T = [False] * self.size  # [False, false, false...., false}
+        L = []  # edge[ vertexIDa , vertexIDb, vertexIDc...]
+        E = []
+
+        # do the algorithm
+        for i in range(self.size):
+            index = 0
+            if i == 0:
+                T[i] = True
+            for j in range(self.size):
+                    if i != j:
+                        if index == 0:
+                            index = [i, j, self.data[i][j]]
+                            y = index[2]
+                        elif y > self.data[i][j]:
+                            index = [i, j, self.data[i][j]]
+                            y = index[2]
+
+            targetEdge = index
+            L.append(targetEdge)
+            #T[targetEdge[0]] = True
+            #T[targetEdge[1]] = True
+            E = []
+
+        # output
+        print(L)
+        length = 0
+        for ele in L:
+            length = length + ele[2]
+        print("MSP length is: ", length-1)
+
+    #works correctly
+    def process3(self):
+        T = [False] * self.size  # [False, false, false...., false}
+        L = []  # edge[ vertexIDa , vertexIDb, vertexIDc...]
+
+        # do the algorithm
+        for i in range(self.size):
+            if i == 0:
+                T[i] = True
+            else:
+                index =-1
+                for j in range(self.size):
+                    for k in range(self.size):
+                        if T[j] != T[k]:
+                            if index == -1:
+                                index = [j, k, self.data[j][k]]
+                                y = index[2]
+                            elif y > self.data[j][k]:
+                                index = [j, k, self.data[j][k]]
+                                y = index[2]
+
+
+                L.append(index)
+                T[index[0]] = True
+                T[index[1]] = True
+                E = []
+
+        # output
+        print(L)
+        length = 0
+        for ele in L:
+            length = length + ele[2]
+        print("MSP length is: ", length)
+
+
+
+
+
+
+
+
 g = Graph()
 x = g.readCSVFiles()
 g.read(x)
-g.process()
+print(timeit.Timer(g.process).timeit(number =10))
+print(timeit.Timer(g.process3).timeit(number =10))
